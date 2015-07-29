589fded5efe9ca73aebafa86fe9085542c1a1772
Alan Hortz <alanhortz@Alans-MacBook-Pro.local>
2015-07-29 12:02:50 +0200

    Add another test

--- app/test/test.js
+++ app/test/test.js
@@ -6,4 +6,9 @@ describe('Array', function(){
       assert.equal(-1, [1,2,3].indexOf(4));
     });
   });
+  describe('length', function(){
+  	it('should be aqual to 3 when the array contains 3 items', function() {
+  		assert.equal(3, [1,2,3].length);
+  	});
+  });
 });
